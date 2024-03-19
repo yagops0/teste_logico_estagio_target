396e032b0fc0b3de23aaf5ff953914b8007599c6
yagops0 <yagosalvador34@gmail.com>
2024-03-19 12:58:00 -0300

    Ajustes

--- alternativa02/main.py
+++ alternativa02/main.py
@@ -1,8 +1,6 @@
 '''
 2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
 '''
-#? soma vira f2
-#? f2 vira f1
 
 def sequencia_fibonacci(numero):
     sequencia = []
